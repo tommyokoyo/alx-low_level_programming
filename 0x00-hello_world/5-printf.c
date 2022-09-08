714e72fd658b008f094bf8e1f746e6388ed5952e
Tommy okoyo <okoyotommy@gmail.com>
2022-09-08 18:30:27 -0400

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - This purpose of this file is to output
- * the string: with proper grammar, but the outcome is a piece of art,
+ * "with proper grammar, but the outcome is a piece of art,
  * followed by a newline, using printf and not puts.
  * return: Always 0 (Success)
  */
