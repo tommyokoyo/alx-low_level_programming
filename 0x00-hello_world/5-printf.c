fa966406fcba778ec31028b1401d54063c323587
Tommy okoyo <okoyotommy@gmail.com>
2022-09-08 18:33:45 -0400

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
 /**
  * main - This purpose of this file is to output
  * "with proper grammar, but the outcome is a piece of art,
- * followed by a newline, using printf and not puts.
+ * followed by a new line
  * return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
