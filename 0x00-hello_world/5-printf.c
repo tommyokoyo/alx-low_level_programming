24579b0a212af8e57d47a4243559768f4a85fe65
Tommy okoyo <okoyotommy@gmail.com>
2022-09-08 18:14:36 -0400

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - This is the entry point
+ * main - This purpose of this file is to output
  * the string: with proper grammar, but the outcome is a piece of art,
  * followed by a newline, using printf and not puts.
  * return: Always 0 (Success)
