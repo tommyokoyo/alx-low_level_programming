5ccb0ae152f3af614293710c2ab6b3db01435048
Tommy okoyo <okoyotommy@gmail.com>
2022-09-10 10:48:50 -0400

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,18 +3,17 @@
 #include <stdlib.h>
 
 /**
- * main: Entry point of the program
- * Program uses the fxn rand() to output
- * positive, negative or zero.
+ * main - This program uses the function rand() to output
+ * random numbers to the screen, then tells the user if it's
+ * positive, negative, or a zero.
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
