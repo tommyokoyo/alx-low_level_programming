a6085cf1fc6027e7ee60bdb90f27c1638f1991e8
Tommy okoyo <okoyotommy@gmail.com>
2022-09-12 22:13:58 -0400

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints numbers
+ * from 0 to 10
+ * Return: Alyways 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	putchar('\n');
+
+	return (0);
+}
