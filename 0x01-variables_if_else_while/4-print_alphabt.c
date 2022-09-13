528daf4faca2910d2d1faa2310025816ba8d53ed
Tommy okoyo <okoyotommy@gmail.com>
2022-09-12 22:09:34 -0400

    prints alphbaets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all the numbers
+ * from a to z with the expection of q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
