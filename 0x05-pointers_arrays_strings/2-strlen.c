abc6e0fddda4f68735be118848d7927de8f1c3f1
Tommy okoyo <okoyotommy@gmail.com>
2022-09-19 14:29:51 -0400

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _srtlen - returns the length of a string
+ * @s: - input string
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int len = strlen(*s);
+
+	return len;
+}
