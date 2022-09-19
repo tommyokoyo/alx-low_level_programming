f66c8ef65d764480c27c725135f4d52ce6f8a1ef
Tommy okoyo <okoyotommy@gmail.com>
2022-09-19 14:40:58 -0400

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: - input
+ * Return: string
+ */
+void _puts(char *str)
+{
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
